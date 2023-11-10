d12a7300d64ffed397f5ff18b1f428a70bce2129
Aidar Asankadyrov <dspace438@gmail.com>
2023-11-10 12:25:53 +0600

    Added new function bro

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/")
 async def say_hello():
     return {"message": "Hello World!"}
+
+
+@app.get("/born/")
+async def say_hello_to_world():
+    return {"message": "Hello World, I was born "}
